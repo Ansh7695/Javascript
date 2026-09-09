a5b6c495b2730d1f7f7d865faf8e3dbac57cdfbc
Ansh Sharma <anshsharma0824@gmail.com>
2026-09-09 12:27:16 +0530

    Add JavaScript day 1 files

---
+++ Day 1 - var,let, const/Temporaldeadzone.js
@@ -0,0 +1,11 @@
+// TEMPORAL DEAD ZONE
+
+//console.log(a);     iska output mei not defined nhi aye kyuki defined h 
+//let a = 12;         but cant access before initiallization
+ 
+// console.log(a);      Isme undifined aayega becoz var use hua h 
+// var a = 12
+
+
+// TDZ mean utna area jisme JS ko pta h variable hai but before initialization 
+// value nhi degi
